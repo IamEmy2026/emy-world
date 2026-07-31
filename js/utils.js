270c577b86c03f6dc4d8981edae8f319c45e0b08
IamEmy2026 <Frikkylord@gmail.com>
2026-07-31 09:50:59 +0100

    Restore EMY OS website and fix HTML errors

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,24 @@
+const Storage = {
+    save(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    },
+
+    load(key, defaultValue) {
+        const data = localStorage.getItem(key);
+
+        if (data === null) {
+            return defaultValue;
+        }
+
+        try {
+            return JSON.parse(data);
+        } catch {
+            return defaultValue;
+        }
+    }
+};
+
+
 // =======================================
 // SAFE ELEMENT FINDER
 // =======================================
